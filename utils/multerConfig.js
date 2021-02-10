e0e486936f90ff1b57045507e0eb7cf045066739
rishabhrishabhchoudhary2496 <rishabh_choudhary@softprodigy.com>
2021-02-10 10:55:21 +0530

    fixed multer bug

--- utils/multerConfig.js
+++ utils/multerConfig.js
@@ -1,14 +1,12 @@
-const wagner = require('wagner-core')
+const multer = require('multer')
 
-module.exports = function storage() {
-  wagner.invoke((multer) => {
-    multer.diskStorage({
-      destination: function (req, file, cb) {
-        cb(null, './public/uploads')
-      },
-      filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
-      },
-    })
-  })
-}
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  },
+})
+
+module.exports = storage
